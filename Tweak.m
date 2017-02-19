659e8165c617ed6fba41fc26de4d9db5a21d82b4
Nosskirneh <andreas.karl.henriksson@gmail.com>
2017-02-19 12:40:05 +0100

    Updated code

--- Tweak.m
+++ Tweak.m
@@ -1,47 +1,14 @@
 @interface SPTPlaylistAddToPlaylistViewController : UIViewController
+- (void)executeSkipAction;
 @end
-
-static BOOL isTargetAlert = NO;
-static SPTPlaylistAddToPlaylistViewController* playlistView;
 
 %hook SPTPlaylistAddToPlaylistViewController
 
-- (void)viewDidAppear:(BOOL)animated {
+
+- (void)executeSkipAction {
     %orig;
     
-    playlistView = self;
-    isTargetAlert = YES;
-}
-
-- (void)viewDidDisappear:(BOOL)animated {
-    %orig;
-    
-    playlistView = nil;
-    isTargetAlert = NO;
-}
-
-%end
-
-
-
-%hook UIAlertAction
-
-+ (instancetype)actionWithTitle:(NSString *)title style:(UIAlertActionStyle)style handler:(void (^)(UIAlertAction *action))handler {
-    if (isTargetAlert) {
-        // Is the desired alert; check if this is the desired action.
-        if ([title isEqualToString:@"Cancel"]) {
-            isTargetAlert = NO;
-            UIAlertAction *newCancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault
-                                                                                   handler:^(UIAlertAction * action) {
-                                                                                       // dismiss ViewController
-                                                                                       [playlistView dismissViewControllerAnimated:YES completion:nil];
-                                                                                   }];
-
-            return newCancelAction;
-        }
-    }
-    
-    return %orig();
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 %end
